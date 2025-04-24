08e09771135dca247e801e944ea8c0aaca7d0086
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-04-25 02:38:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/shared/Navbar'
+import Sidebar from './components/Sidebar'
+import Body from './components/Body'
+import Inbox from './components/Inbox'
+import Mail from './components/Mail'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Body/>,
+    children:[
+      {
+        path:"/",
+        element:<Inbox/>
+      },
+      {
+        path:"/mail/:id",
+        element:<Mail/>
+      }
+    ]
+  }
+])
 
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    </>
+    <div className='bg-[#F6f8FC] h-screen w-screen overflow-hidden'>
+      <Navbar />
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { IoMdStar } from 'react-icons/io'
+import { LuPencil } from 'react-icons/lu'
+import { MdOutlineDrafts, MdOutlineKeyboardArrowDown, MdOutlineWatchLater } from 'react-icons/md'
+import { TbSend2 } from 'react-icons/tb'
+
+const sidebarItems = [
+    {
+        icons: <LuPencil size={"24px"} />,
+        text: "Inbox"
+    },
+    {
+        icons: <IoMdStar size={"24px"} />,
+        text: "Starred"
+    },
+    {
+        icons: <MdOutlineWatchLater size={"24px"} />,
+        text: "Snoozed"
+    },
+    {
+        icons: <TbSend2 size={"24px"} />,
+        text: "Sent"
+    },
+    {
+        icons: <MdOutlineDrafts size={"24px"} />,
+        text: "Drafts"
+    },
+    {
+        icons: <MdOutlineKeyboardArrowDown size={"24px"} />,
+        text: "More"
+    },
+
+]
+
+const Sidebar = () => {
+    return (
+        <div className='w-[15%]'>
+            <div className='p-3'>
+                <button className='flex items-center gap-2 p-4 rounded-2xl hover:shadow bg-[#C2E7FF] '>
+                    <LuPencil size={"24px"} /> Compose
+                </button>
+            </div>
+            <div className='text-gray-500'>
+                {
+                    sidebarItems.map((item, index) => {
+                        return (
+                            <div className='flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer my-2 hover:bg-gray-200'>
+                                {item.icons}
+                                <p>{item.text}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
